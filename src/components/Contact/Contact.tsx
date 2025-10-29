fb24da8f644bbebc52f2a6f554bd2e98032891f6
moatasim <mo32000a@gmail.com>
2025-10-29 13:29:47 +0300

    added hero gsap

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -34,7 +34,7 @@ const Contact = ({
                     <SocialUl />
                 </div>
                 <div className={styles.location}>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.4954545057676!2d16.346496200000004!3d48.197072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d078b9f7daadd%3A0xf4cc95071955a277!2sMedizin%20Mariahilf!5e0!3m2!1sen!2seg!4v1758353796303!5m2!1sen!2seg" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d170130.3807588384!2d16.214834562549303!3d48.22034409583409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d079e5136ca9f%3A0xfdc2e58a51a25b46!2sVienna%2C%20Austria!5e0!3m2!1sen!2seg!4v1761733194174!5m2!1sen!2seg" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div className={styles.address}>
                     <span>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -17,6 +17,8 @@ import 'swiper/css/pagination';
 import { Link } from '@/i18n/navigation'
 import { useTranslations } from 'next-intl'
 
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 const Hero = ({
   lo
 } : {
@@ -24,7 +26,31 @@ const Hero = ({
 }) => {
 
     const t = useTranslations("HomePage.Hero")
-
+    useGSAP(() => {
+        gsap.from(".stagger-text", {
+            y: 100,
+            opacity: 0,
+            // ease: "circ",
+            duration: 0.8, 
+            delay: 1.3,
+            // stagger: 0.1 // this means the delat betweem the staggered elememts is 0.1
+            stagger: {
+                amount: 0.4,
+                grid: [1,1],
+                axis: "y",
+                // ease: "circ.inOut",
+                // from: "random"
+            }
+        })
+        gsap.from("#hero-img", {
+            x: 80,
+            opacity: 0,
+            filter: "blur(40px)",
+            // ease: "circ",
+            duration: 0.6,
+            delay: 1.3
+        })
+     }, [])
   return (
     <section className={lo === "ar" ? styles.hero + " " + styles.ar : styles.hero} id='hero'>
         <div className={styles.swiper}>
@@ -50,22 +76,24 @@ const Hero = ({
                 >
                 <SwiperSlide className={styles.slide + ` relative min-h-[80vh] flex items-center bg-neutral-100 overflow-hidden`}>
                     <div className={styles.text}>
-                        <Image src={logo} alt='Logo'></Image>
-                        <h1>
+                        <Image className='stagger-text' src={logo} alt='Logo'></Image>
+                        <h1 className='stagger-text'>
                             {t("heading")}
                         </h1>
 
-                        <p>
+                        <p className='stagger-text'>
                             {t("description")}
                         </p>
-
-                        <Link
-                            href="/"
-                        >
-                        {t("cta")}
-                        </Link>
+                        <div className='stagger-text'>
+                            <Link
+                                
+                                href="/"
+                            >
+                            {t("cta")}
+                            </Link>
+                        </div>
                     </div>
-                        <Image src={heroImg} alt='Brain Image'></Image>
+                        <Image id='hero-img' src={heroImg} alt='Brain Image'></Image>
                 </SwiperSlide>
             </Swiper>
             <div className={styles.heroPagination}>
